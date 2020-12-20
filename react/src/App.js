0386d6d290feeedc38451ee35f5c8354b8c124ed
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2020-12-21 02:57:53 +0300

    Add files via upload

---
+++ react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p>
+        Done by Safonov N.M. from IKBO-04-17
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
